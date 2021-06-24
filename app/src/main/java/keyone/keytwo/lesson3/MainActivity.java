ef0cf148930fc8294729023dcadcf50058df96ec
Natalia <nef.n@list.ru>
2021-06-24 16:37:54 +0300

    Создали плохой, но рабочий OnClick
    Описание юююю

--- app/src/main/java/keyone/keytwo/lesson3/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson3/MainActivity.java
@@ -1,14 +1,91 @@
 package keyone.keytwo.lesson3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    Button button1;
+    int counter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState==null){
+            makeToast("первый запуск onCreate()");
+        }else{
+            makeToast("повторный запуск onCreate()");
+        }
+        textView1 = findViewById(R.id.textView1);
+        button1 = findViewById(R.id.button1);
+        counter1 = 0;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        makeToast("onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause()");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        makeToast("onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy()");
+    }
+
+    private void makeToast(String toast) {
+        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+        Log.d("my log", toast);
+    }
+
+    public void button1_onclick(View view) {
+        counter1++;
+        textView1.setText(counter1+"");
     }
 }
